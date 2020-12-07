28961dd5450f15dc3d1f703e7f410f2721a50a37
Cameron Robinson <robinsoncameron1103@@gmail.com>
2020-12-07 17:39:54 -0600

    Finished Guess My Number

--- IntroToC++/Name.cpp
+++ IntroToC++/Name.cpp
@@ -4,19 +4,18 @@
 
 int main() 
 {
-	std::string help;
-
-	int max = 100;
 	int min = 0;
+	int max = 100;
 	int tries = 0;
+	std::string help;
 
 	std::cout << "Think of a number between 1 and 100." << std::endl;
 
 	do 
 	{
-		int guess = 20 % (max - (min + 1)) + (min + 1);
+		int guess = 50 % (max - (min + 1)) + (min + 1);
 		std::cout << "I guess " << guess << std::endl;
-		std::cout << "Am I guess high, low, or correct?: ";	
+		std::cout << "Am I guessing too high, too low, or am I right? " << std::endl;
 		std::cin >> help;
 
 		if (help == "high") 
@@ -26,13 +25,13 @@ int main()
 			tries++;
 		}
 
-		if (help == "low")
+		else if (help == "low")
 		{ 
 			std::cout << "I was too low." << std::endl;
 			min = guess;
 			tries++;
 		}
-		if (tries == 5)
+		else if (tries == 100)
 		{ 
 			std::cout << "Hey.... did you change your number?" << std::endl;
 		}
@@ -44,5 +43,4 @@ int main()
 
 	return(0);
 
-	//int guess = 2 % (max - (min + 1)) + (min + 1);
 } 
