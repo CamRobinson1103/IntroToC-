3e42e71a0b064457c1e0a3ccafad2ee8631abc37
Cameron Robinson <robinsoncameron1103@@gmail.com>
2020-12-09 21:16:28 -0600

    Made Tic Tac Toe

--- IntroToC++/Name.cpp
+++ IntroToC++/Name.cpp
@@ -1,46 +1,150 @@
 #include <iostream>
 
+char space[10] = { 'o','1','2','3','4','5','6','7','8','9' };
 
+int checkWin();
+void grid();
 
-int main() 
+int main()
 {
-	int min = 0;
-	int max = 100;
-	int tries = 0;
-	std::string help;
+    int player = 1, i, choice;
 
-	std::cout << "Think of a number between 1 and 100." << std::endl;
+    char mark;
+    do
+    {
+        grid();
+        player = (player % 2) ? 1 : 2;
 
-	do 
-	{
-		int guess = 50 % (max - (min + 1)) + (min + 1);
-		std::cout << "I guess " << guess << std::endl;
-		std::cout << "Am I guessing too high, too low, or am I right? " << std::endl;
-		std::cin >> help;
+        std::cout << "Player " << player << ", enter a number:  ";
+        std::cin >> choice;
 
-		if (help == "high") 
-		{ 
-			std::cout << "I was too high." << std::endl;
-			max = guess;
-			tries++;
-		}
+        mark = (player == 1) ? 'x' : 'o';
 
-		else if (help == "low")
-		{ 
-			std::cout << "I was too low." << std::endl;
-			min = guess;
-			tries++;
-		}
-		else if (tries == 100)
-		{ 
-			std::cout << "Hey.... did you change your number?" << std::endl;
-		}
-	}
+        if (choice == 1 && space[1] == '1')
+        {
+            space[1] = mark;
+        }
+
+        else if (choice == 2 && space[2] == '2')
+        {
+            space[2] = mark;
+        }
+
+        else if (choice == 3 && space[3] == '3')
+        {
+            space[3] = mark;
+        }
+
+        else if (choice == 4 && space[4] == '4')
+        {
+            space[4] = mark;
+        }
+
+        else if (choice == 5 && space[5] == '5')
+        {
+            space[5] = mark;
+        }
+
+        else if (choice == 6 && space[6] == '6')
+        {
+            space[6] = mark;
+        }
+
+        else if (choice == 7 && space[7] == '7')
+        {
+            space[7] = mark;
+        }
+
+        else if (choice == 8 && space[8] == '8')
+        {
+            space[8] = mark;
+        }
+
+        else if (choice == 9 && space[9] == '9')
+        {
+            space[9] = mark;
+        }
+
+        else
+        {
+            std::cout << "Movement Unavailable ";
+        }
+        i = checkWin();
+
+        player++;
+    } while (i == -1);
+    grid();
+    if (i == 1)
+
+        std::cout << "==>\aPlayer " << --player << " win ";
+    else
+        std::cout << "==>\aTie";
+
+    std::cin.ignore();
+    std::cin.get();
+    return 0;
+}
+
+int checkWin()
+{
+    if (space[1] == space[2] && space[2] == space[3])
+    {
+        return 1;
+    }
+    else if (space[4] == space[5] && space[5] == space[6])
+    {
+        return 1;
+    }
+    else if (space[7] == space[8] && space[8] == space[9])
+    {
+        return 1;
+    }
+    else if (space[1] == space[4] && space[4] == space[7])
+    {
+        return 1;
+    }
+    else if (space[2] == space[5] && space[5] == space[8])
+    {
+        return 1;
+    }
+    else if (space[3] == space[6] && space[6] == space[9])
+    {
+        return 1;
+    }
+    else if (space[1] == space[5] && space[5] == space[9])
+    {
+        return 1;
+    }
+    else if (space[3] == space[5] && space[5] == space[7])
+    {
+        return 1;
+    }
+
+    else if (space[1] != '1' && space[2] != '2' && space[3] != '3'
+        && space[4] != '4' && space[5] != '5' && space[6] != '6'
+        && space[7] != '7' && space[8] != '8' && space[9] != '9')
+    {
+        return 0;
+    }
 
 
-	while (help != "correct");
-	std::cout << "The computer's guess was correct after " << tries << " tries!" << std::endl;
 
-	return(0);
+    else
+    {
+        return -1;
+    }
 
-} 
+}
+
+void grid()
+{
+    system("cls");
+
+    std::cout << "Player 1 = x    Player 2 = o   Press Enter after x or o" << std::endl;
+
+
+    std::cout << "  " << space[1] << "  ,  " << space[2] << "  ,  " << space[3] << std::endl;
+    std::cout << "  " << space[4] << "  ,  " << space[5] << "  ,  " << space[6] << std::endl;
+    std::cout << "  " << space[7] << "  ,  " << space[8] << "  ,  " << space[9] << std::endl;
+
+}
